56cca26b15b05da9d5578ef33ef2d70afc531322
Megɑn ♡ <megan43@hawaii.edu>
2022-01-31 03:16:33 -1000

    modified css and js

--- art128/base-site-singlepagesite/js/script.js
+++ art128/base-site-singlepagesite/js/script.js
@@ -11,7 +11,18 @@ document.addEventListener('DOMContentLoaded', function() {
             //else (if false) do this:
             menunav.setAttribute('data-navstate', 'open');
         };
-    });
+
+});
+
+    const sitenav = document.querySelector(".site-nav");
+
+    menubutton.onclick = function () {
+        if (sitenav.getAttribute("data-navstate") === "open") {
+            sitenav.setAttribute("data-navstate", "closed");
+        } else {
+            sitenav.setAttribute("data-navstate", "open");
+        }
+    };
 
     //STICKY NAV LINKS TOGGLE MENU NAV
     var stickynavlinks = document.querySelectorAll(".sticky nav a");
