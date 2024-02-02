64e2c3b3ef34d6e503e4223e9e1d0c1f2a4fe930
Megɑn ♡ <megan43@hawaii.edu>
2024-02-01 15:50:29 -1000

    added swiper js to archive

--- art285/portfolio-v1b/js/script.js
+++ art285/portfolio-v1b/js/script.js
@@ -1,6 +1,22 @@
-// var swiper = new Swiper(".mySwiper", {
-//     pagination: {
-//         el: ".swiper-pagination",
-//         dynamicBullets: true,
-//     },
-//     });
+var swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  centeredSlides: true,
+  initialSlide: 2,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 10,
+    initialSlide: 3,
+    slideShadows: true
+  },
+  pagination: {
+    el: ".swiper-pagination",
+     clickable: true
+  },
+  autoplay: true,
+  loop: true
+});
